59802f6f36ec360ca33c35d25ce9d6640f5be96d
beldaryogita <109028443+beldaryogita@users.noreply.github.com>
2024-03-14 12:08:44 +0530

    Add files via upload

---
+++ assingnment_3/src/in/acts/cdac/First.java
@@ -0,0 +1,13 @@
+package in.acts.cdac;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n =123;
+		double n1 = n;
+		System.out.println(n1);
+
+	}
+
+}
